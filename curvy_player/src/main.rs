87a0c3b8e4ec72321e6486b3fd5f69d4f0860564
Snorre Vandbakk Fjelde <snorre.vandbakk.fjelde@oneco.no>
2026-05-12 21:43:20 +0200

    Made sine waves work, now gotta deal with real files

--- curvy_player/src/main.rs
+++ curvy_player/src/main.rs
@@ -1,43 +1,46 @@
-use curvy_wav::*;
-use curvy_core::{AudioSample, AudioStream as CurvyAudio};
+use std::f64::consts::TAU;
+use raylib::core::audio::RaylibAudio;
+use raylib::ffi::IsAudioStreamProcessed;
+use raylib::ffi::UpdateAudioStream;
 
-use std::time::{Instant, Duration};
-use raylib::ffi::LoadAudioStream;
-use raylib::core::audio::{RaylibAudio, AudioStream};
-
-const BUFFER_SIZE: i32 = 800;
+const FREQ: f64 = 440.0;
+const SAMPLE_RATE: u32 = 44100;
+const BUFFER_SIZE: i32 = 4096;
+const AMP: f64 = 0.2; // volume (0.0–1.0)
 
 fn main() {
-    let mut wav = WavStream::from_file("curvy_wav/samples/Alesis-Fusion-Shakuhachi-C5.wav").unwrap();
-    wav.play();
+    let ray_audio = RaylibAudio::init_audio_device().unwrap();
+    ray_audio.set_audio_stream_buffer_size_default(BUFFER_SIZE);
+    let mut stream = ray_audio.new_audio_stream(SAMPLE_RATE, 16, 1);
+    stream.play();
+    let stream = unsafe { stream.inner() };
 
+    let mut theta = 0.0;
+    let step = TAU * FREQ / SAMPLE_RATE as f64;
+    let mut buf: Vec<i16> = vec![0; BUFFER_SIZE as usize];
 
-    let audio_device = RaylibAudio::init_audio_device().unwrap();
-    audio_device.set_audio_stream_buffer_size_default(BUFFER_SIZE);
-
-    let mut audio_stream = audio_device.new_audio_stream(
-        wav.sample_rate(), 
-        wav.sample_size(), 
-        1
-    );
-    //println!("Sample size: {}", wav.sample_size());
-    //panic!();
-    
-    audio_stream.play();
-    let mut buf = vec![0; BUFFER_SIZE as usize];
-    audio_stream.update(&buf);
     loop {
-        //println!("Here");
-        if audio_stream.is_processed() {
-            for i in 0..BUFFER_SIZE {
-                if let Some(sample) = wav.sample() {
-                    if let AudioSample::PCM16(val) = sample {
-                        buf[i as usize] = val;
-                    }
+        let is_processed = unsafe {
+            IsAudioStreamProcessed(stream)
+        };
+        if is_processed {
+            for s in &mut buf {
+                let sine = f64::sin(theta);
+                *s = (sine * AMP * i16::MAX as f64) as i16;
+
+                theta += step;
+                if theta >= TAU {
+                    theta -= TAU;
                 }
             }
 
-            audio_stream.update(&buf);
+            unsafe {
+                UpdateAudioStream(
+                    stream, 
+                    buf.as_ptr() as *const std::os::raw::c_void, 
+                    BUFFER_SIZE
+                );
+            }
         }
     }
 }

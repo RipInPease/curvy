8920e5508221a62751228f380f463adad18890ae
RipInPease <snorre.fjelde@gmail.com>
2026-05-20 21:34:31 +0200

    Fucking around with cpal

--- curvy_player/src/main.rs
+++ curvy_player/src/main.rs
@@ -1,12 +1,10 @@
 use std::io::{Read, BufReader};
 use std::fs;
 
-use sdl3::audio::{AudioFormat as SdlAudioFormat, AudioSpec, AudioStream as SdlAudioStream};
+use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
 
 use curvy_core::*;
 use curvy_wav::WavStream;
-
-const BUFFER_SIZE: i32 = 4410;
 
 
 enum AudioFormat<R: Read> {
@@ -47,19 +45,48 @@ impl<R: Read> AudioStream for AudioFormat<R> {
 
 
 fn main() {
-    let mut audio_format = match open_audio() {
+    let mut audio_format = match open_audio_file() {
         Some(v) => v,
         None    => return
     };
 
-    
-    let sdl_context = sdl3::init().expect("Failed to init SDL3");
-    let audio_system = sdl_context.audio().expect("Failed to create audio system using SDL3");
-    
+    let cpal_host = cpal::default_host();
+    let device = cpal_host.default_output_device().expect("Not output device available");
+
+    let config = cpal::StreamConfig {
+        channels: audio_format.num_chs() as u16,
+        sample_rate: audio_format.sample_rate(),
+        buffer_size: cpal::BufferSize::Default,
+    };
+
+    let sample_format = match audio_format.sample_size() {
+        8   => cpal::SampleFormat::U8,
+        16  => cpal::SampleFormat::I16,
+        32  => cpal::SampleFormat::F32,
+        b   => {
+            println!("Unknown sample format: {b} bits per sample");
+            return
+        }
+    };
+
+    let stream = device.build_output_stream(
+        &config, 
+        move |data: &mut [i16], info: &cpal::OutputCallbackInfo| {
+            // react to stream events and read or write stream data here.
+            update_audio_buffer(data, info, &mut audio_format);
+        },
+        move |err| {
+            // react to errors here.
+        },
+        None // None=blocking, Some(Duration)=timeout
+    ).expect("Failed to open audio stream");
+
+    stream.play().unwrap();
+    loop {}
 }
 
 
-fn open_audio() -> Option<AudioFormat<BufReader<fs::File>>> {
+fn open_audio_file() -> Option<AudioFormat<BufReader<fs::File>>> {
     use std::env;
     let mut args = env::args();
     args.next();
@@ -90,4 +117,23 @@ fn open_audio() -> Option<AudioFormat<BufReader<fs::File>>> {
     };
 
     Some(audio_format)
+}
+
+
+fn update_audio_buffer<S>(data: &mut [i16], _: &cpal::OutputCallbackInfo, audio_stream: &mut S) 
+where
+    S: AudioStream,
+{
+    let mut i = 0;
+    let i_max = data.len();
+
+    while i < i_max && let Some(frame) = audio_stream.frame() {
+        for sample in frame.samples() {
+            if let AudioSample::PCM16(val) = sample {
+                data[i] = *val;
+                //println!("sample: {}", val);
+            }
+            i += 1;
+        }
+    }
 }
